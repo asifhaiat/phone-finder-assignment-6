7904753f07e11d3f00b359c70e2d5a9b8a708c70
asifhaiat <asifhaiat67465@gmail.com>
2022-03-02 12:15:30 +0600

    added display arrow function

--- phone.js
+++ phone.js
@@ -1,3 +1,4 @@
+// added search phone by arrow function
 const searchPhone = () => {
     const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
@@ -10,3 +11,24 @@ const searchPhone = () => {
         .then((res) => res.json())
         .then((data) => displayPhone(data.data.slice(0, 20)));
     };
+// display phone 
+const displayPhone = (data) => {
+    // console.log(data);
+  const searchPhone = document.getElementById("phone-search");
+  data.forEach((phone) => {
+      // console.log(data);
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+          <div class="card">
+                  <img src="${phone.image}" class="card-img-top alt="...">
+                  <div class="card-body">
+                  <h5 class="card-title">${phone.phone_name}</h5>
+                  <p class="card-text">${phone.brand}</p>
+                  <button onclick="LoadPhoneDetails('${phone.slug}')" type="button" class="btn btn-primary btn-sm">Details</button>
+              </div>
+          </div>
+          `;
+      searchPhone.appendChild(div);
+  });
+  };
